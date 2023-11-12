457c7b9a4015d52bbfdf1664203bfe24003ee2e0
Artem Shadrin <explay97@gmail.com>
2023-11-13 00:23:27 +0500

    Updates

---
+++ static/js/439.d04b33ed.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmarvel=self.webpackChunkmarvel||[]).push([[439],{8439:(e,t,n)=>{n.r(t),n.d(t,{default:()=>a});var l=n(9613),s=n(1523),i=n(184);const a=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)(l.Z,{}),(0,i.jsx)("p",{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px"},children:"Page doesn't exist"}),(0,i.jsx)(s.rU,{style:{display:"block",textAlign:"center",fontWeight:"bold",fontSize:"24px",marginTop:"30px"},to:"/",children:"Back to main page"})]})}}]);
+//# sourceMappingURL=439.d04b33ed.chunk.js.map
