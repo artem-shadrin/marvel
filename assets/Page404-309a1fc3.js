06bedd23e662ba303d96792b1ae234c821bdb302
Artem Shadrin <explay97@gmail.com>
2023-11-13 00:41:38 +0500

    Updates

---
+++ assets/Page404-309a1fc3.js
@@ -0,0 +1,1 @@
+import{j as e,E as t,L as s}from"./index-df1f222d.js";const i=()=>e.jsxs("div",{children:[e.jsx(t,{}),e.jsx("p",{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px"},children:"Page doesn't exist"}),e.jsx(s,{style:{display:"block",textAlign:"center",fontWeight:"bold",fontSize:"24px",marginTop:"30px"},to:"/",children:"Back to main page"})]});export{i as default};
